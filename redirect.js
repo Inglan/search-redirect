d780931835fc2bf696ce3106c3d4ac96ba74509f
Ingo Wolf <me@ingowolf.au>
2025-07-03 08:51:23 +1000

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,9 @@
+/// google.js
+if (location.pathname == "/search") {
+  location.host = "google.com"
+}
+
+/// brave.js
+if (location.pathname == "/search") {
+  location.host = "search.brave.com"
+}
